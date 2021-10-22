abadc487063208b65248547b6a2121cb9f8dab52
Rhogar <johnfmartinez597@gmail.com>
2021-10-22 19:21:56 -0400

    Changed app title in index.html added character arrays & window prompts

--- Develop/assets/script/script.js
+++ Develop/assets/script/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+// Arrays for different password parameters
+function generatePassword() {
+  var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+// Store user selections
+  var resultArray = [];
+  var userArray = [];
+// Window alerts 
+  var charCount = prompt("How long do you want your password to be?", "Range: 8 to 128");
+  var optNumber = confirm("Do you want numbers?");
+  var upperLetters = confirm("Do you want Uppercases?");
+  var lowerLetters = confirm("Do you want Lowercases?");
+  var
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
